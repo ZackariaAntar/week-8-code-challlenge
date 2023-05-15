4c69e1221777c103738f083c5dd97a1afb0cf8d6
Zackaria Antar <antarzack@gmail.com>
2023-05-15 13:50:05 -0500

    set up serverside get and post routes and functions.

--- server/server.js
+++ server/server.js
@@ -37,6 +37,37 @@ let jokes = [
 // serve back static files
 app.use(express.static('server/public'));
 
+
+app.get('/jokes', function(req, res){
+  res.send(jokes)
+
+
+})
+
+
+app.post('/jokes', function (req, res){
+  jokes.push(req.body);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
 }); // end spin up server
