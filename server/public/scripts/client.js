fd18fc6dc8260a4507f23a8b6be6d1d24860ff35
Zackaria Antar <antarzack@gmail.com>
2023-05-15 14:06:26 -0500

    structured client ajax post.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -20,13 +20,44 @@ function getJokes(){
 }
 
 function postJokes(){
+    let whoseJoke = $("#whoseJokeIn").val();
+    let jokeQuestion = $("#questionIn").val();
+    let punchLine = $("#punchlineIn").val();
+    $.ajax({
+        method: 'POST',
+        url: '/jokes',
+        data: {
+            whoseJoke,
+            jokeQuestion,
+            punchLine,
+        }
+    }).then(function(response){
+        getJokes()
+    }).catch(function(error){
+        console.log("Whoopsies, couldn't post what you were looking for", error);
+
+    })
 
 }
 
-function renderToDOM(){
+function renderToDOM(jokeList){
+    for (let joke of jokeList){
+        $('#outputDiv').append(<p class="jokes">${joke.jokeQuestion}</p>)
+        // add the rest of the props after testing.
+        // figure out formatting.
+    }
 
 }
 
+
+
+
+//     <input type='text' id='whoseJokeIn' placeholder='whose joke' />
+    // <input type='text' id='questionIn' placeholder='question' />
+    // <input type='text' id='punchlineIn' placeholder='punchline' />
+    // <button id='addJokeButton'>Add Joke</button>
+    // <h2>Jokes:</h2>
+    // <div id='outputDiv'></div>
 
 
 
