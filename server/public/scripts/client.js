4163e6f377b7e90b955317372042490bfbf43cbf
Zackaria Antar <antarzack@gmail.com>
2023-05-15 14:27:03 -0500

    completes client and server logic.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,70 +1,71 @@
-console.log('client.js sourced');
+console.log("client.js sourced");
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 let whoseJoke;
 let jokeQuestion;
 let punchLine;
 
 function onReady() {
-    $("#addJokeButton").on('click', postJokes);
-    console.log('DOM ready');
-    getJokes()
+	$("#addJokeButton").on("click", postJokes);
+	console.log("DOM ready");
+	getJokes();
 }
 
-function getJokes(){
-    $.ajax({
-        method: 'GET',
-        url: '/jokes'
-    }).then(function(response){
-        renderToDOM(response)
-    }).catch(function(error){
-        console.log('Whoopsies, couldn\'t get what you were looking for', error);
-    })
-
+function getJokes() {
+	$.ajax({
+		method: "GET",
+		url: "/jokes",
+	})
+		.then(function (response) {
+			renderToDOM(response);
+		})
+		.catch(function (error) {
+			console.log(
+				"Whoopsies, couldn't get what you were looking for",
+				error
+			);
+		});
 }
 
-function postJokes(event){
-    event.preventDefault()
-    whoseJoke = $("#whoseJokeIn").val();
-    jokeQuestion = $("#questionIn").val();
-    punchLine = $("#punchlineIn").val();
-    $.ajax({
-        method: 'POST',
-        url: '/jokes',
-        data: {
-            whoseJoke,
-            jokeQuestion,
-            punchLine,
-        }
-    }).then(function(response){
-        getJokes()
-    }).catch(function(error){
-        console.log("Whoopsies, couldn't post what you were looking for", error);
-
-    })
-
+function postJokes(event) {
+	event.preventDefault();
+	whoseJoke = $("#whoseJokeIn").val();
+	jokeQuestion = $("#questionIn").val();
+	punchLine = $("#punchlineIn").val();
+	$.ajax({
+		method: "POST",
+		url: "/jokes",
+		data: {
+			whoseJoke,
+			jokeQuestion,
+			punchLine,
+		},
+	})
+		.then(function (response) {
+			getJokes();
+		})
+		.catch(function (error) {
+			console.log(
+				"Whoopsies, couldn't post what you were looking for",
+				error
+			);
+		});
 }
 
-function renderToDOM(jokeList){
-    $('#outputDiv').empty()
-    for (let joke of jokeList){
-        $('#outputDiv').append(`<p class="jokes">${joke.jokeQuestion}</p>`)
-        // add the rest of the props after testing.
-        // figure out formatting.
-    }
+function renderToDOM(jokeList) {
+	$("#whoseJokeIn").val("");
+	$("#questionIn").val("");
+	$("#punchlineIn").val("");
+	$("#outputDiv").empty();
+	for (let joke of jokeList) {
+		$("#outputDiv").append(`
+        <div class="full-joke">
+            <p class="jokes">Question: ${joke.jokeQuestion}</p>
+            <p class="jokes">Punchline: ${joke.punchLine}</p>
+            <p class="jokes">-Submitted by: ${joke.whoseJoke}</p>
+        </div>
 
+        `);
+	}
 }
-
-
-
-
-//     <input type='text' id='whoseJokeIn' placeholder='whose joke' />
-    // <input type='text' id='questionIn' placeholder='question' />
-    // <input type='text' id='punchlineIn' placeholder='punchline' />
-    // <button id='addJokeButton'>Add Joke</button>
-    // <h2>Jokes:</h2>
-    // <div id='outputDiv'></div>
-
-
-
--- server/server.js
+++ server/server.js
@@ -1,75 +1,53 @@
-const express = require( 'express' );
+const express = require("express");
 const app = express();
-const bodyParser = require( 'body-parser' );
+const bodyParser = require("body-parser");
 const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let jokes = [
-  {
-    whoseJoke: "Danny",
-    jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat!"
-  },
-  {
-    whoseJoke: "Luke",
-    jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
-  },
-  {
-    whoseJoke: "Millie",
-    jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
-  },
-  {
-    whoseJoke: "dEv",
-    jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs!"
-  },
-  {
-    whoseJoke: "Scott",
-    jokeQuestion: "I went to the zoo the other day, it had one dog...",
-    punchLine: "It was a shih tzu."
-  }
+	{
+		whoseJoke: "Danny",
+		jokeQuestion: "Why do scuba divers fall backwards out of boats?",
+		punchLine: "If they fell forwards they’d still be in the boat!",
+	},
+	{
+		whoseJoke: "Luke",
+		jokeQuestion:
+			"Two fish are in a tank. What did one fish say to the other?",
+		punchLine: "Do you know how to drive this thing?",
+	},
+	{
+		whoseJoke: "Millie",
+		jokeQuestion: "What do you call a pile of cats?",
+		punchLine: "A meowntain!",
+	},
+	{
+		whoseJoke: "dEv",
+		jokeQuestion: "Why should you not play cards in the forest?",
+		punchLine: "Too many Cheetahs!",
+	},
+	{
+		whoseJoke: "Scott",
+		jokeQuestion: "I went to the zoo the other day, it had one dog...",
+		punchLine: "It was a shih tzu.",
+	},
 ];
 
 // serve back static files
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
+app.get("/jokes", function (req, res) {
+	res.send(jokes);
+});
 
-app.get('/jokes', function(req, res){
-  res.send(jokes)
+app.post("/jokes", function (req, res) {
+	jokes.push(req.body);
 
-
-})
-
-
-app.post('/jokes', function (req, res){
-  jokes.push(req.body);
-
-  res.sendStatus(201)
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
+	console.log("server running on: ", PORT);
 }); // end spin up server
