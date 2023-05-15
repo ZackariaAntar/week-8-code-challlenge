58a436e889da86b1979f99a28e5d050e7e1e09e9
Zackaria Antar <antarzack@gmail.com>
2023-05-15 13:54:25 -0500

    set up ajax get call on client.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,29 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    getJokes()
 }
+
+function getJokes(){
+    $.ajax({
+        method: 'GET',
+        url: '/jokes'
+    }).then(function(response){
+        renderToDOM(response)
+    }).catch(function(error){
+        console.log('Whoopsies, couldn\'t get what you were looking for', error);
+    })
+
+}
+
+function postJokes(){
+
+}
+
+function renderToDOM(){
+
+}
+
+
+
+
